ae9fa8b8f5f524943492784d45205e5b642a3a91
DataShoaib <mdshoaiba478@gmail.com>
2025-05-15 18:49:30 +0530

    updated

--- text_utils.py
+++ text_utils.py
@@ -1,23 +1,35 @@
-# text_utils.py
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-from bs4 import BeautifulSoup
+
 import string
+from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-
+# Safe way to download only if not present
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
 
 def clean_text(text):
+    # Remove HTML tags
     text = BeautifulSoup(text, "html.parser").get_text()
+
+    # Lowercase
     text = text.lower()
+
+    # Remove punctuation
     text = text.translate(str.maketrans('', '', string.punctuation))
+
+    # Tokenize
     tokens = word_tokenize(text)
+
+    # Remove non-alphanumeric tokens (optional, as punctuation is already removed)
     tokens = [word for word in tokens if word.isalnum()]
+
+    # Remove stopwords
     stop_words = set(stopwords.words('english'))
     tokens = [word for word in tokens if word not in stop_words]
+
+    # Stemming
     stemmer = PorterStemmer()
     stemmed = [stemmer.stem(word) for word in tokens]
 
